9ca9b36272648326c25674511071243d455d180c
shigzh <shigaozhan@sfn.cn>
2019-05-15 14:52:25 +0800

    提交shop项目

--- microservice-parent/microservice-consumer/src/main/java/cn/sgz/springcloud/config/ConfigBean.java
+++ microservice-parent/microservice-consumer/src/main/java/cn/sgz/springcloud/config/ConfigBean.java
@@ -43,9 +43,12 @@ public class ConfigBean {
      */
     @Bean
     public IRule ribbonRule() {
-        //return new BestAvailableRule();//选择一个最小的并发请求的server
+        //return new BestAvailableRule();//选择一个最小的并发请求的server(会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的服务)
         return new RoundRobinRule();//轮询
         //return new RandomRule();//随机
-        //return new WeightedResponseTimeRule();//根据响应时间分配一个weight，响应时间越长，weight越小，被选中的可能性越低。
+        //return new AvailabilityFilteringRule();//会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，还有并发的连接数量超过阈值的服务，然后对剩余的服务列表按照轮询策略进行访问
+        //return new RetryRule();//先按照RoundRobinRule（轮询）的策略获取服务，如果获取服务失败则在指定时间内会进行重试;则在失效时间deadline之前不断的进行重试（重试时获取服务的策略还是RoundRobinRule中定义的策略），获取可用的服务
+        //return new WeightedResponseTimeRule();//根据平均响应时间计算所有服务的权重，响应时间越快服务权重越大，被选中的概率越高；刚启动时如果统计信息不足，则使用轮询策略，等统计信息足够，会切换到WeightedResponseTimeRule
+        //return new ZoneAvoidanceRule();//(Finchley.SR1版本中默认均衡策略,目前G版本采用的是轮询策略；复合判断server所在区域的性能和server的可用性来选择服务器
     }
 }
